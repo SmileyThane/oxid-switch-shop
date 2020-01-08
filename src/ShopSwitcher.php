4930be196a31ddcf4e74313348eac78597e2ff32
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2020-01-08 21:13:54 +0100

    //for what reason will the shoplist not be a array?

--- src/ShopSwitcher.php
+++ src/ShopSwitcher.php
@@ -17,12 +17,18 @@ class ShopSwitcher implements IteratorAggregate
     /**
      * @var array<string> $shopList
      **/
-    private $shopList = ['1'];
+    private $shopList;
    
     public function __construct()
     {
         $shopListService = oxNew(ShopList::class);
         $fullShopList = $shopListService->getAll();
+        if (! is_array($fullShopList)) {
+            $this->shopList = ['1'];
+            //for what reason will the shoplist not be a array?
+            vardump($fullShopList);
+            return;
+        }
         $this->shopList = array_map(
             function ($shop) {
                 return $shop->getId();
