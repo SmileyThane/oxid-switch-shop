6d64ae5d2b2c745cc14c9d8f8380a6395a68169f
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2020-01-08 11:35:18 +0100

    code style

--- src/ShopSwitcher.php
+++ src/ShopSwitcher.php
@@ -27,7 +27,8 @@ class ShopSwitcher implements IteratorAggregate
             $fullShopList,
             function($shop) {
                 return $shop->getId();
-            });
+            }
+        );
     }
    
     /**
